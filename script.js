83d3c0aa7e038ed3320a780ea0fa6c0234dcac5d
PabsFerdinand89 <pabloantoniohernandez@live.com>
2023-04-20 13:51:37 -0500

    added event handlers and modified date function

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
 //API ID= 163
 
-
 const todaysDate = function () {
   let today = new Date();
   let dd = String(today.getDate()).padStart(2, '0');
   let mm = String(today.getMonth() + 1).padStart(2, '0');
   let yyyy = today.getFullYear();
-  
-  getElementById($('#listed-tasks')).innerHTML = `Today is ${dd}/${mm}/${yyyy}`;
 
-  //how do you get today's date in Javascript?*
-
-
-
-  
+  $('#currentDate').innerHTML = `Today is ${dd}/${mm}/${yyyy}`;
   };
 
+var filterTasks = function () {
+  $(this).addClass('active');
+  $(this).siblings().removeClass('active');
+  getAndShowAllTasks();
+}
 
-  var getAndShowAllTasks = function (){
+var getAndShowAllTasks = function (){
   $.ajax({
     type: 'GET',
     URL: 'https://fewd-todolist-api.onrender.com/tasks?api_key=163',
@@ -42,107 +40,104 @@ const todaysDate = function () {
   };
 
 //new task
-  var newTasks = function () {
-    $.ajax({
-      type: 'POST',
-      URL: 'https://fewd-todolist-api.onrender.com/tasks/:id?api_key=163',
-      content: 'application/json',
-      dataType: 'json',
-      data: JSON.stringify({
+var newTask = function () {
+  $.ajax({
+    type: 'POST',
+    URL: 'https://fewd-todolist-api.onrender.com/tasks/:id?api_key=163',
+    content: 'application/json',
+    dataType: 'json',
+    data: JSON.stringify({
       task: {
-        content: $('#newTask').val(),
-        },
+        content: $('#new-task').val();
+        getAndShowAllTasks();
+        }
       }),
-      success: function (response, textStatus) {
-        getAndShowAllTasks();},
-      Error: function (request, textStatus, errorMessage) {
-        console.log(errorMessage);
-      }
-    });
-  };
-
-//submitted task
-  var tasksSubmitted = function () {
-    let inputTheTask = $('.new-to-do input');
-
-    $.ajax({
-      type: 'POST',
-      url:'https://fewd-todolist-api.onrender.com/tasks?api_key=163',
-      dataType: 'application/json',
-      success: function (response, textStatus) {
-        filterAllResults();},
-      Error: function (request, textStatus, errorMessage) {
-        console.log(errorMessage);
-      }
-    });
-  };
+    success: function (response, textStatus) {
+      $('#new-task').val('');
+      getAndShowAllTasks();
+    },
+    Error: function (request, textStatus, errorMessage) {
+      console.log(errorMessage);
+    },
+  });
+}; 
 
 //delete task
-  var Tasksdeleted = function () {
-    $.ajax({
-      type: 'DELETE',
-      url: 'https://fewd-todolist-api.onrender.com/tasks/:id?api_key=163',
-      dataType: 'json',
-      success: function (response, textStatus) {
-        getAndShowAllTasks();
-      },
-      Error: function (request, textStatus, errorMessage) {
-        console.log(errorMessage);
-      }
+var TaskDeleted = function (id) {
+  $.ajax({
+    type: 'DELETE',
+    url: 'https://fewd-todolist-api.onrender.com/tasks/' + id + '?api_key=163',
+    success: function (response, textStatus) {
+      getAndShowAllTasks();
+    },
+    Error: function (request, textStatus, errorMessage) {
+      console.log(errorMessage);
+    },
+  });
+};
+
+//task completed
+var tasksCompleted = function (id) {
+  $.ajax({
+    type: 'PUT',
+    url:'https://fewd-todolist-api.onrender.com/tasks/' + id + '/mark_complete?api_key=163',
+    data:'json',
+    success: function (response, textStatus) {
+      getAndShowAllTasks();
+    },
+    Error: function (request, textStatus, errorMessage) {
+      console.log(errorMessage);
+    },
+  });
+};  
+
+//task active
+var tasksIsActive = function (id) {
+  $.ajax({
+    type: 'PUT',
+    url: 'https://fewd-todolist-api.onrender.com/tasks/' + id + '/mark_active?api_key=163',
+    dataType: 'json',
+    Success: function (response, textStatus) {
+      console.log('API request successful');
+      getAndShowAllTasks();
+    },
+    Error: function (request, textStatus, errorMessage) {
+      console.log(errorMessage);
+    },
+  });
+};
+
+
+$(document).ready(function (){
+  //show all tasks
+  $('#all-button').on('click', function () {
+    $('.listed-items').each(function (i,e) {
+      $(e).show();
     });
-  };
+    $(this).addClass('selectedTasks');
+    $(this).siblings().removeClass('selectedTasks');
+  })
 
-//
-  var tasksCompleted = function (id) {
-    $.ajax({
-      type: 'PUT',
-      url:'https://fewd-todolist-api.onrender.com/tasks/:id/mark_complete?api_key=163',
-      data:'json',
-      success: function (response, textStatus) {
-        getAndShowAllTasks();
-      },
-      Error: function (request, textStatus, errorMessage) {
-        console.log(errorMessage);
-      }
-    });
-  };
+  //newTask event handler
+  $('#new-task').on('submit', function (e) {
+    e.preventDefault();
+    newTask();
+  });
 
-//
-  var tasksIsActive = function (id) {
-    $.ajax({
-      type: 'PUT',
-      url: 'https://fewd-todolist-api.onrender.com/tasks/:id/mark_active?api_key=163',
-      data: 'json',
-      success: function (response, textStatus) {
-        getAndShowAllTasks();
-      },
-      Error: function (request, textStatus, errorMessage) {
-        console.log(errorMessage);
-      }
-    });
-  };
+  //taskDeleted event handler
+  $(document).on('click', '.delete', function (){
+    TaskDeleted($(this).data('id'));
+  });
 
-  getAndShowAllTasks();
-
-
-$(document).ready(function () {
-
-  getAndShowAllTasks();
-  
-  $(document).on('click', 'select', function (){
+  //event for checked/unchecked checkbox
+  $(document).on('change', '.mark-complete', function (){
     if (this.checked) {
-      taskCompleted($(this).data.('id'));
-    } else {
-      taskIsActive($(this).data.('id'));
+      tasksCompleted($(this).data('id'));
+      } else {
+      tasksIsActive($(this).data('id'));
     }
   });
 
-  $(document).on('click', 'delete', function ({
-    
-  })
-
-  $(document).on('submit', function (e) {
-    e.preventDefault();
-    taskSubmitted()  ;
-  })
+  todaysDate();
+  getAndShowAllTasks();
 });
